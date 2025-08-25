8e210a97446b7bb91e591fbb588a595e567f7d6a
AlecFrame <walteravertacnik@sanluis.edu.ar>
2025-08-24 22:09:43 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace vertacnik_inmobiliaria2025.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
